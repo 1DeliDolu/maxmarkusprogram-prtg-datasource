53439886e7eff3bed05df0e9363b189fb3a9077a
Mustafa Özdemir <mustafa.ozdemir1408@gmail.com>
2025-02-02 14:47:56 +0100

    utils ordner is  deleted

--- src/utils/encryption.ts
+++
@@ -1,8 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-const SECRET_KEY = 'PRTG_GRAFANA_SECRET'; 
-
-export const encryptPassword = (password: string): string => {
-    console.log('Encrypting password: ', CryptoJS.AES.encrypt(password, SECRET_KEY).toString());
-  return CryptoJS.AES.encrypt(password, SECRET_KEY).toString();
-};
